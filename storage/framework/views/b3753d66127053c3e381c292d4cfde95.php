c24c45b718eedc9cdc6ccb110736d8ee002bf931
eyadalalimi2 <eyadads.inc@gmail.com>
2025-09-01 05:32:04 +0300

    chore: ignore Laravel compiled views/logs/cache and untrack existing

--- storage/framework/views/b3753d66127053c3e381c292d4cfde95.php
+++
@@ -1,7 +0,0 @@
-<footer class="py-4 bg-light">
-  <div class="container d-flex flex-wrap justify-content-between align-items-center">
-    <span class="text-muted">© <?php echo e(date('Y')); ?> Campus</span>
-    <a href="<?php echo e(route('admin.dashboard')); ?>" class="small">لوحة التحكم</a>
-  </div>
-</footer>
-<?php /**PATH C:\xampp\htdocs\campus\resources\views/site/partials/footer.blade.php ENDPATH**/ ?>
