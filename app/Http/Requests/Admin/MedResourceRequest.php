e1555a41aa63193aefedaabc583b09a21f1ebb90
eyadalalimi2 <eyadads.inc@gmail.com>
2025-11-21 02:33:22 +0300

    test

--- app/Http/Requests/Admin/MedResourceRequest.php
+++ app/Http/Requests/Admin/MedResourceRequest.php
@@ -16,7 +16,7 @@ class MedResourceRequest extends FormRequest
             'category_id' => ['required','integer','exists:med_resource_categories,id'],
             'title' => ['required','string','max:255'],
             'description' => ['nullable','string'],
-            'file' => ['nullable','file','mimes:pdf','max:30000'], // 30MB
+            'file' => ['nullable','file','mimes:pdf','max:102400'], // 100MB
             'file_url' => ['nullable','url','max:1000'],
             'file_size_bytes' => ['nullable','integer','min:0'],
             'pages_count' => ['nullable','integer','min:1'],
